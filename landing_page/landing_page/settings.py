c3b458da73629e4fe77d83783e69614e195020d5
Willem <singer.willem@gmail.com>
2023-01-19 18:11:34 +0000

    First commit

--- landing_page/landing_page/settings.py
+++ landing_page/landing_page/settings.py
@@ -29,7 +29,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
