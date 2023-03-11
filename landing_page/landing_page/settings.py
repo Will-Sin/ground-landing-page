2132e312c2bacdb2c213164f4f13f35a7818b45f
Willem <singer.willem@gmail.com>
2023-03-11 18:00:45 +0000

    Merged Static Files solution. Still problem with having to be in development mode to True.

--- landing_page/landing_page/settings.py
+++ landing_page/landing_page/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = 'static/'
+# Uncomment for production
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = 'static/'
 
 STATIC_ROOT = "/Users/wet-virus/PycharmProjects/DjangoTest/firstproject/static"
 STATIC_URL = "landing/static/"
