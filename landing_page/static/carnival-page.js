bdfedbc3ac12d9b974749ed7dcc3410991b10897
Willem <singer.willem@gmail.com>
2023-06-20 14:17:47 +0100

    Book ID verification

--- landing_page/static/carnival-page.js
+++ landing_page/static/carnival-page.js
@@ -10,6 +10,7 @@ var sendButton = document.getElementsByClassName("send-text-message")[0];
 var inputField = document.getElementsByClassName("text-input")[0];
 var bookIDObject = document.getElementsByClassName("book-id")[0];
 var messageBoxDiv = document.getElementsByClassName("message-box-div");
+var bookVerifyBox = document.getElementsByClassName("book-verify-box");
 var interactionsCountDiv = document.getElementsByClassName("interactions")[0];
 var consoleDiv = document.getElementsByClassName("console")[0];
 var bookError = document.getElementsByClassName("book-error")[0];
@@ -29,7 +30,7 @@ function checkInputLength() {
     console.log('Four characters entered!');
 
     bookError.classList.remove("hide");
-    interactionVerify(value)
+    BookAndInteractionVerify(value)
     // You can add more code or call another function
   }
 
@@ -48,6 +49,18 @@ function update_DOM(input, user) {
 
 };
 
+function bookVerify(response_object) {
+  if response_object["Book ID"] == "Invalid" {
+    //Error message about Offering String
+    messageBoxDiv[0].classList.remove("hide");
+    return
+  }
+  else {
+    //Success message about Offering String
+    messageBoxDiv[0].classList.add("hide");
+  }
+}
+
 /** Called once a voice note is sent and determines what do to given how many interactions are returned*/
 function interactionCalculator(response_object) {
   interactionsCount = response_object["interactions_available"];
@@ -56,16 +69,16 @@ function interactionCalculator(response_object) {
 
   if (interactionsCount <= 0) {
 
-    messageBoxDiv[0].classList.add("hide");
+    messageBoxDiv[0].classList.remove("hide");
     messageBoxDiv[1].classList.add("hide");
 
     return 0
   } else {
 
-    messageBoxDiv[0]
+    messageBoxDiv[0].classList.add("hide");
     messageBoxDiv[1].classList.remove("hide");
 
-    interactionsCountDiv.innerHTML = `<i>You now can ask <b>${interactionsCount}</b> questions to the Oracle. After those are up, you'll need to wait until the next scenario.</i>`
+    interactionsCountDiv.innerHTML = `<i>You now can ask <b>${interactionsCount}</b> questions to the Oracle. After those are up, you'll need a new offering.</i>`
 
     return 1
   };
@@ -93,8 +106,9 @@ async function fetchChat(user_input, chat_history) {
     };
 
   // GREEN and 1 are place holders because API needs them, but it doesn't affect anything.
-  return fetch(`${productionURL}/script/${bookID}/GREEN/1/`, requestOptions)
+  return fetch(`${productionURL}/book/${bookID}/GREEN/1/`, requestOptions)
 };
+
 
 /** A fetch function that sends the inputed scenario ID and returns whether there is a script to post or not*/
 async function fetchScript(bookID) {
@@ -118,7 +132,7 @@ async function fetchScript(bookID) {
   };
 
   // GREEN and 1 are place holders because API needs them, but it doesn't affect anything.
-  return fetch(`${productionURL}/script/${bookID}/GREEN/1/`, requestOptions)
+  return fetch(`${productionURL}/book/${bookID}/GREEN/1/`, requestOptions)
 };
 
 /** Called once the send button is clicked and sends the inputed text to the fetch function that calls the Oracle Endpoint*/
@@ -145,7 +159,7 @@ async function sendChat() {
 
 
 /** Called once the scenario is changed or selected, and checks if there needs to be script for the Oracle or not*/
-async function interactionVerify(bookID) {
+async function BookAndInteractionVerify(bookID) {
 
     let oracle_name = "Oracle";
 
@@ -154,6 +168,8 @@ async function interactionVerify(bookID) {
     response_object = await res.json();
 
     consoleDiv.innerHTML = JSON.stringify(response_object)
+
+    bookVerify(response_object)
 
     interactionCalculator(response_object)
 
