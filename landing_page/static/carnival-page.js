4a8ce2b7055c32b913eecbc89c889323c5f72f36
Willem <singer.willem@gmail.com>
2023-07-04 17:06:46 -0400

    Console removed

--- landing_page/static/carnival-page.js
+++ landing_page/static/carnival-page.js
@@ -326,6 +326,7 @@ async function caveVerify() {
     }
 
     //consoleDiv.innerHTML = JSON.stringify(response_object)
+    //consoleDiv.innerHTML = JSON.stringify(response_object)
 
     if (bookVerify(response_object)) {
       console.log("error 3")
