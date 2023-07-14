2b4b5550e168f94e9124e25792287c3cc0c6e56f
Willem <singer.willem@gmail.com>
2023-07-14 10:55:10 +0100

    Testing page added

--- landing_page/pages/urls.py
+++ landing_page/pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('landing-page/', views.landingPage, name='landing-page'),
-    path('testing-page/', views.landingPage, name='testing-page'),
+    path('testing-page/', views.testingPage, name='testing-page'),
     path('carnival/', views.indexCarnival, name='carnival-page'),
 ]
